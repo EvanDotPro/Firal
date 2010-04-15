b981c11cc83c13f66a67a3265700a7d7eeed4435
Evan Coury <me@evancoury.com>
2010-04-14 17:39:12 -0700

    updated schema to prepare for multi-site stuff and add-ons

--- data/db/mysql.sql
+++ data/db/mysql.sql
@@ -1,27 +1,65 @@
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    name VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    role VARCHAR(20) NOT NULL,
-    UNIQUE (
-       name
-    )
-);
-CREATE TABLE config (
-    name VARCHAR(50) NOT NULL ,
-    value VARCHAR(200) NOT NULL ,
-    PRIMARY KEY ( name )
-);
-CREATE TABLE pages (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    author INT NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    content LONGTEXT NOT NULL,
-    UNIQUE (
-        name
-    ),
-    INDEX author (
-        author
-    )
-)
+CREATE TABLE `addon` (
+  `addon_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`addon_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `config` (
+  `name` varchar(50) NOT NULL,
+  `value` varchar(200) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `page` (
+  `page_id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `content` longtext NOT NULL,
+  PRIMARY KEY (`page_id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `author` (`author`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `perspective` (
+  `perspective_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `parent_perspective_id` int(11) DEFAULT NULL,
+  `session_group_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`perspective_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `perspective_access` (
+  `perspective_access_id` int(11) NOT NULL AUTO_INCREMENT,
+  `host` varchar(255) NOT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `perspective_id` int(11) NOT NULL,
+  `ssl_available` tinyint(4) NOT NULL,
+  PRIMARY KEY (`perspective_access_id`),
+  UNIQUE KEY `host_path` (`host`,`path`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `perspective_addon` (
+  `perspective_id` int(11) NOT NULL,
+  `addon_id` int(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  UNIQUE KEY `perspective_addon` (`perspective_id`,`addon_id`),
+  KEY `weight` (`weight`),
+  KEY `perspective_id` (`perspective_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `session_group` (
+  `session_group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `perspective_access_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`session_group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
