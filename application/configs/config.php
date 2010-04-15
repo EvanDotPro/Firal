042597f43cdf00f4ea9a34eadcd9f98f9d543f27
Evan Coury <me@evancoury.com>
2010-04-14 18:15:05 -0700

    switched to my method of config files... much better

--- application/configs/config.php
+++ application/configs/config.php
@@ -1,40 +1,29 @@
 <?php
-return array_merge_recursive(array(
-    'bootstrap' => array(
-        'path'  => APPLICATION_PATH . '/Bootstrap.php',
-        'class' => 'Bootstrap'
-    ),
-    'autoloaderNamespaces' => array(
-        'Firal_'
-    ),
-    'resources'   => array(
-        'frontController' => array(
-            'moduleDirectory'       => APPLICATION_PATH . '/modules',
-            'prefixDefaultModule'   => true
-        ),
-        'modules' => array(),
-        'view' => array(),
-        'layout' => array(
-            'layout'      => 'layout',
-            'pluginClass' => 'Firal_Controller_Plugin_Layout',
-            'layoutPath'  => APPLICATION_PATH . '/layouts/scripts'
-        ),
-        'db' => array(
-            'adapter' => 'pdo_mysql'
-        ),
-        'cachemanager' => array(
-            'database' => array(
-                'frontend' => array(
-                    'name'    => 'Core',
-                    'options' => array(
-                        'automatic_serialization' => true
-                    )
-                ),
-                'backend'  => array()
-            )
-        ),
-        'plugin' => array(
-            'path' => APPLICATION_PATH . '/plugins'
-        )
-    )
-), include dirname(__FILE__) . '/' . APPLICATION_ENV . '.config.php');
+$config = array();
+
+$config['bootstrap']['path']     = APPLICATION_PATH . '/Bootstrap.php';
+$config['bootstrap']['class']    = 'Bootstrap';
+
+$config['autoloaderNamespaces'][] = 'Firal_';
+
+$config['resources']['frontController']['moduleDirectory']      = APPLICATION_PATH . '/modules';
+$config['resources']['frontController']['prefixDefaultModule']  = true;
+
+$config['resources']['modules'] = array();
+$config['resources']['view'] = array();
+
+$config['resources']['layout']['layout']        = 'layout';
+$config['resources']['layout']['pluginClass']   = 'Firal_Controller_Plugin_Layout';
+$config['resources']['layout']['layoutPath']    = APPLICATION_PATH . '/layouts/scripts';
+
+$config['resources']['db']['adapter'] = 'pdo_mysql';
+
+$config['resources']['cachemanager']['database']['frontend']['name'] = 'Core';
+$config['resources']['cachemanager']['database']['frontend']['options']['automatic_serialization'] = true;
+$config['resources']['cachemanager']['database']['backend'] = array();
+
+$config['resources']['plugin']['path'] = APPLICATION_PATH . '/plugins';
+
+include dirname(__FILE__) . '/' . APPLICATION_ENV . '.config.php';
+
+return $config;
