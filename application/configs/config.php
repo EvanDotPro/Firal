1079f32bf57ff7e166f782e15df1e4e8873fa56e
Pieter Kokx <pieter@kokx.nl>
2010-05-05 19:08:16 +0200

    Not all config files need to be .dist files.

--- application/configs/config.php
+++ application/configs/config.php
@@ -1,9 +1,10 @@
 <?php
-return array_merge_recursive(array(
-    'autoloaderNamespaces' => array(
-        'Firal_'
-    )
-),
-include dirname(__FILE__) . '/config.' . APPLICATION_MODE . '.php',
-include dirname(__FILE__) . '/config.' . APPLICATION_MODE . '.' . APPLICATION_ENV . '.php'
+return array_merge_recursive(
+    array(
+        'autoloaderNamespaces' => array(
+            'Firal_'
+        )
+    ),
+    include dirname(__FILE__) . '/config.' . APPLICATION_MODE . '.php',
+    include dirname(__FILE__) . '/config.' . APPLICATION_MODE . '.' . APPLICATION_ENV . '.php'
 );
